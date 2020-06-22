4d4dc626815fe5b01208f0a7c7fd8cfc53c78590
Stricks1 <gmalheiross@gmail.com>
2020-06-22 18:16:32 -0300

    fix linter error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,7 +23,7 @@ class PostsController < ApplicationController
     us = current_user.friends.map(&:id)
     us << current_user.id
     us = us.join(',')
-    first_part = "SELECT * FROM posts WHERE user_id IN"
+    first_part = 'SELECT * FROM posts WHERE user_id IN'
     @timeline_posts = Post.find_by_sql("#{first_part} (SELECT id FROM users WHERE id IN(#{us})) ORDER BY created_at")
   end
 
