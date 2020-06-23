cce751bfd2dfad11437127794de36d1a76085429
queeksm <queeksm@gmail.com>
2020-06-23 15:38:06 -0500

    Added the inverse friendship functionality, now 2 inverse entries are created on the friendship table when a friendship is accepted, also implemented the tests for it on Rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,8 +39,13 @@ class User < ApplicationRecord
 
   def confirm_friend(user)
     friendship = inverse_friendships.find { |friend| friend.user == user }
+    friendship2 = friendships.build
+    friendship2.user_id = self.id
+    friendship2.friend_id = user.id
+    friendship2.confirmed = true
     friendship.confirmed = true
     friendship.save
+    friendship2.save
   end
 
   def reject_friend(user)
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -20,5 +20,16 @@ RSpec.describe Friendship, type: :model do
       f = u.friendships.first
       expect(f.confirmed).to eq(true)
     end
+
+    it 'Inverse friendship' do
+      @user.request_friend(@user2)
+      @user2.confirm_friend(@user)
+      u = User.find(@user.id)
+      u2 = User.find(@user2.id)
+      f = u.friendships.first
+      f2 = u2.friendships.first
+      expect(f.user_id).to eq(f2.friend_id)
+      expect(f2.user_id).to eq(f.friend_id)
+    end
   end
 end
